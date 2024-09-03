5e56f8ccd4a9badf67bb591d665a1d2de3fedae3
sadeg-akbarian <sadeg.akbarian@gmail.com>
2024-09-03 17:18:45 +0200

    feat: unzip-method created but extracted file can still not be displayed

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,9 @@
-<script setup>
-import MainComponent from "./components/MainComponent.vue";
-</script>
-
 <template>
   <MainComponent msg="CSV-Dateien vergleichen" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import MainComponent from "./components/MainComponent.vue";
+</script>
+
+<style scoped></style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,63 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <input type="file" accept=".zip" @change="loadTheFile($event)" />
+  <p></p>
+</template>
+
 <script>
+import JSZip from "jszip";
+
 export default {
   data() {
-    return {};
+    return {
+      csvContent: "",
+    };
+  },
+  methods: {
+    loadTheFile(event) {
+      const file = event.target.files[0];
+      console.log(file);
+
+      if (file) {
+        console.log(file.size);
+        let theSize = file.size;
+        if (theSize < 1024) {
+          theSize = `${theSize} Bytes`;
+        } else if (theSize < 1024 * 1024) {
+          theSize = `${(theSize / 1024).toFixed(2)} KB`;
+        } else {
+          theSize = `${(theSize / (1024 * 1024)).toFixed(2)} MB`;
+        }
+        console.log(theSize);
+
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          const arrayBuffer = e.target.result;
+          const jszip = new JSZip();
+          jszip
+            .loadAsync(arrayBuffer)
+            .then((zip) => {
+              zip.forEach((relativePath, file) => {
+                file.async("string").then((content) => {
+                  // Ausgabe des Dateinamens und Inhalts
+                  // console.log("Datei:", relativePath);
+                  // console.log("Inhalt:", content);
+                  // console.log(typeof content);
+                  const theParagraph = document.querySelector("p");
+                  theParagraph.innerText = "content";
+                  const blob = new Blob([content], { type: "text/plain" });
+                  const sizeInBytes = blob.size;
+                  console.log(sizeInBytes);
+                });
+              });
+            })
+            .catch((err) => {
+              console.error("Fehler beim Entpacken der ZIP-Datei:", err);
+            });
+        };
+        reader.readAsArrayBuffer(file);
+      }
+    },
   },
   props: {
     msg: String,
@@ -9,8 +65,8 @@ export default {
 };
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-</template>
-
-<style scoped></style>
+<style scoped>
+div {
+  margin-top: 5rem;
+}
+</style>
