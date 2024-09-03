91c7057e28bab821af9f89c82bf443dd1053ecf8
sadeg-akbarian <sadeg.akbarian@gmail.com>
2024-09-03 13:24:09 +0200

    initial commit

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped></style>
