b86d3f58616aa3e0112b853283f791c360ccbd4d
sadeg-akbarian <sadeg.akbarian@gmail.com>
2024-09-07 20:06:25 +0200

    feat: vinyls are now displayed in a table

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,7 +1,25 @@
 <template>
   <h1>{{ msg }}</h1>
   <input type="file" accept=".zip" @change="loadTheFile($event)" />
-  <p></p>
+  <p>{{ calculateSizeInMB }}</p>
+  <table v-if="finalBase.length > 10">
+    <tr v-for="(vinyl, index) of finalBase" :key="index">
+      <template v-if="index < 10000">
+        <td>{{ vinyl[0] }}</td>
+        <td>{{ vinyl[1] }}</td>
+        <td>{{ vinyl[2] }}</td>
+        <td>{{ vinyl[3] }}</td>
+        <td>{{ vinyl[4] }}</td>
+        <td>{{ vinyl[5] }}</td>
+        <td>{{ vinyl[6] }}</td>
+        <td>{{ vinyl[7] }}</td>
+        <td>{{ vinyl[8] }}</td>
+        <td>{{ vinyl[9] }}</td>
+        <td>{{ vinyl[11] }}</td>
+        <td>{{ vinyl[12] }}</td>
+      </template>
+    </tr>
+  </table>
 </template>
 
 <script>
@@ -12,13 +30,28 @@ export default {
     return {
       csvContent: "",
       theDatabases: [],
+      newDatabase: null,
+      finalBase: [],
     };
+  },
+  computed: {
+    calculateSizeInMB() {
+      // Das JSON-Objekt in einen String umwandeln
+      const jsonString = JSON.stringify(this.finalBase);
+      // Die Länge des Strings berechnen
+      const byteSize = new Blob([jsonString]).size;
+      // Die Größe in Megabytes umwandeln
+      const sizeInMB = byteSize / (1024 * 1024);
+      return "It is " + sizeInMB + " MB big!";
+    },
   },
   methods: {
     loadTheFile(event) {
       const file = event.target.files[0];
       console.log(file);
       const mainThis = this;
+      mainThis.newDatabase = null;
+      mainThis.finalBase = [];
 
       if (file) {
         const reader = new FileReader();
@@ -46,7 +79,80 @@ export default {
                       previousLastNumberOfDatabases + 1;
                     mainThis.theDatabases.push(newLastNumberOfDatabases);
                   }
-                  // TODO: Füge indexedDB hinzu
+                  const regex = /(?=\d{9},)/g;
+                  mainThis.newDatabase = content.split(regex);
+
+                  for (let platte of mainThis.newDatabase) {
+                    // Split-Kommas, die nicht innerhalb von Anführungszeichen liegen
+                    const result = platte.match(/(".*?"|[^",]+)(?=\s*,|\s*$)/g);
+
+                    if (result) {
+                      // Entferne die umschließenden Anführungszeichen aus den Ergebnissen
+                      const cleanedResult = result.map((item) =>
+                        item.trim().replace(/^"(.*)"$/, "$1")
+                      );
+                      const shortenedCleanedResult = cleanedResult.slice(0, 13);
+                      mainThis.finalBase.push(shortenedCleanedResult);
+                    } else {
+                      console.error("No match found for:", platte);
+                    }
+                  }
+
+                  // TODO: Im mainThis.finalBase müssen die items in key-value-Paare umgewandelt werden, bei denen der key die ID ist
+                  // TODO: und der value ein object, der die restlichen Daten der Platte enthält
+
+                  // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+                  // const request = indexedDB.open("CSV-Databases", 1);
+                  // request.onupgradeneeded = (event) => {
+                  //   mainThis.newDatabase = event.target.result;
+                  //   if (
+                  //     !mainThis.newDatabase.objectStoreNames.contains("myStore")
+                  //   ) {
+                  //     mainThis.newDatabase.createObjectStore("myStore", {
+                  //       keyPath: "id",
+                  //       autoIncrement: true,
+                  //     });
+                  //   }
+                  // };
+                  // request.onsuccess = (event) => {
+                  //   mainThis.newDatabase = event.target.result;
+                  //   console.log("Database opened successfully");
+                  //   console.log(event);
+                  //   console.log(event.target);
+
+                  //   // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+                  //   const transaction = mainThis.newDatabase.transaction(
+                  //     ["myStore"],
+                  //     "readwrite"
+                  //   );
+                  //   const store = transaction.objectStore("myStore");
+                  //   store.add({ content });
+
+                  //   transaction.oncomplete = () => {
+                  //     console.log("Data added");
+                  //     const transaction = mainThis.newDatabase.transaction(
+                  //       ["myStore"],
+                  //       "readonly"
+                  //     );
+                  //     const store = transaction.objectStore("myStore");
+                  //     const request = store.get(1);
+
+                  //     request.onsuccess = (event) => {
+                  //       // console.log(event.target.result.content);
+                  //       const regex = /(?=\d{9},)/g;
+                  //       mainThis.finalBase =
+                  //         event.target.result.content.split(regex);
+                  //     };
+                  //   };
+
+                  //   // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+                  // };
+
+                  // request.onerror = (event) => {
+                  //   console.error("Database error:", event.target.errorCode);
+                  // };
                 });
               });
             })
@@ -67,5 +173,15 @@ export default {
 <style scoped>
 div {
   margin-top: 5rem;
+}
+
+table {
+  margin-top: 3rem;
+  border-collapse: collapse;
+}
+
+table,
+td {
+  border: 2px solid black;
 }
 </style>
