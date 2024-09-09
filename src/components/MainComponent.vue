0cfb8816273988a288e9504747dae7b3895ceebd
sadeg-akbarian <sadeg.akbarian@gmail.com>
2024-09-09 19:45:57 +0200

    feat & refactor: now 2 input-Elements are implemented with surrounding divs and ID's added to inputs, second table added in html for display of second cvs-file, h2-Elements added for both tables in html, new data properties added for the methods, second computed property added for display of size of second cvs-file, in loadTheFile-function now first check if both downloaded files are the same or not and rest of function now implemented for both input-Elements, second deleteButton added for second input-Element, clearFile-function now implemented for both deleteButtons

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,11 +1,51 @@
 <template>
   <h1>{{ msg }}</h1>
-  <input type="file" accept=".zip" @change="loadTheFile($event)" />
-  <p>{{ calculateSizeInMB }}</p>
-  <button type="button" @click="clearFile">Delete File 1</button>
-  <button type="button">Delete File 2</button>
+  <div class="input-area">
+    <div class="first-input-area">
+      <label for="firstInput">Lade die <span>erste</span> CSV-Datei</label>
+      <input
+        id="firstInput"
+        type="file"
+        accept=".zip"
+        @change="loadTheFile($event)"
+      />
+    </div>
+    <div class="second-input-area">
+      <label for="secondInput">Lade die <span>zweite</span> CSV-Datei</label>
+      <input
+        id="secondInput"
+        type="file"
+        accept=".zip"
+        @change="loadTheFile($event)"
+      />
+    </div>
+  </div>
+  <p>{{ calculateFirstSizeInMB }}</p>
+  <p>{{ calculateSecondSizeInMB }}</p>
+  <button type="button" @click="clearFile('first')">Delete File 1</button>
+  <button type="button" @click="clearFile('second')">Delete File 2</button>
+  <h2>First File</h2>
   <table v-if="firstFinalBase.length > 10">
     <tr v-for="(vinyl, index) of firstFinalBase" :key="index">
+      <template v-if="index < 10">
+        <td>{{ vinyl[0] }}</td>
+        <td>{{ vinyl[1] }}</td>
+        <td>{{ vinyl[2] }}</td>
+        <td>{{ vinyl[3] }}</td>
+        <td>{{ vinyl[4] }}</td>
+        <td>{{ vinyl[5] }}</td>
+        <td>{{ vinyl[6] }}</td>
+        <td>{{ vinyl[7] }}</td>
+        <td>{{ vinyl[8] }}</td>
+        <td>{{ vinyl[9] }}</td>
+        <td>{{ vinyl[11] }}</td>
+        <td>{{ vinyl[12] }}</td>
+      </template>
+    </tr>
+  </table>
+  <h2>Second File</h2>
+  <table v-if="secondFinalBase.length > 10">
+    <tr v-for="(vinyl, index) of secondFinalBase" :key="index">
       <template v-if="index < 10">
         <td>{{ vinyl[0] }}</td>
         <td>{{ vinyl[1] }}</td>
@@ -32,28 +72,71 @@ export default {
     return {
       csvContent: "",
       theDatabases: [],
-      fileInputElement: null,
+      firstFileName: "",
+      firstInputElement: null,
+      secondFileName: "",
+      secondInputElement: null,
       firstNewDatabase: null,
       firstFinalBase: [],
+      secondNewDatabase: null,
+      secondFinalBase: [],
     };
   },
   computed: {
-    calculateSizeInMB() {
+    calculateFirstSizeInMB() {
       // Das JSON-Objekt in einen String umwandeln
       const jsonString = JSON.stringify(this.firstFinalBase);
       // Die Länge des Strings berechnen
       const byteSize = new Blob([jsonString]).size;
       // Die Größe in Megabytes umwandeln
       const sizeInMB = byteSize / (1024 * 1024);
-      return "It is " + sizeInMB + " MB big!";
+      return "The first CSV-File is " + sizeInMB + " MB big!";
+    },
+    calculateSecondSizeInMB() {
+      // Das JSON-Objekt in einen String umwandeln
+      const jsonString = JSON.stringify(this.secondFinalBase);
+      // Die Länge des Strings berechnen
+      const byteSize = new Blob([jsonString]).size;
+      // Die Größe in Megabytes umwandeln
+      const sizeInMB = byteSize / (1024 * 1024);
+      return "The first CSV-File is " + sizeInMB + " MB big!";
     },
   },
   methods: {
     loadTheFile(event) {
+      const mainThis = this;
+      console.log(event.target.files[0].name);
+
+      if (
+        event.target.id === "firstInput" &&
+        event.target.files[0].name === this.secondFileName
+      ) {
+        alert(
+          "You choose this file already for the other input. Please select a different file!"
+        );
+        event.target.value = "";
+        return;
+      } else if (
+        event.target.id === "secondInput" &&
+        event.target.files[0].name === this.firstFileName
+      ) {
+        alert(
+          "You choose this file already for the other input. Please select a different file!"
+        );
+        event.target.value = "";
+        return;
+      }
+
       const file = event.target.files[0];
       console.log(file);
-      const mainThis = this;
-      mainThis.fileInputElement = event.target;
+
+      if (event.target.id === "firstInput") {
+        mainThis.firstInputElement = event.target;
+        mainThis.firstFileName = event.target.files[0].name;
+      } else {
+        mainThis.secondInputElement = event.target;
+        mainThis.secondFileName = event.target.files[0].name;
+      }
 
       if (file) {
         const reader = new FileReader();
@@ -66,26 +149,20 @@ export default {
               zip.forEach((relativePath, file) => {
                 file.async("string").then((content) => {
                   console.log("Datei:", relativePath);
-                  // console.log("Inhalt:", content);
                   console.log(typeof content);
-                  if (!window.indexedDB) {
-                    alert("Sorry! Your browser does not support IndexedDB");
-                  }
                   console.log(mainThis.theDatabases);
-                  if (mainThis.theDatabases.length === 0) {
-                    mainThis.theDatabases.push(1);
-                  } else {
-                    const previousLastNumberOfDatabases =
-                      mainThis.theDatabases[mainThis.theDatabases.length - 1];
-                    let newLastNumberOfDatabases =
-                      previousLastNumberOfDatabases + 1;
-                    mainThis.theDatabases.push(newLastNumberOfDatabases);
-                  }
                   const regex = /(?=\d{9},)/g;
-                  mainThis.firstNewDatabase = content.split(regex);
-                  // console.log();
 
-                  for (let platte of mainThis.firstNewDatabase) {
+                  let whichNewDataBase;
+                  if (event.target.id === "firstInput") {
+                    mainThis.firstNewDatabase = content.split(regex);
+                    whichNewDataBase = mainThis.firstNewDatabase;
+                  } else {
+                    mainThis.secondNewDatabase = content.split(regex);
+                    whichNewDataBase = mainThis.secondNewDatabase;
+                  }
+
+                  for (let platte of whichNewDataBase) {
                     // Split-Kommas, die nicht innerhalb von Anführungszeichen liegen
                     const result = platte.match(/(".*?"|[^",]+)(?=\s*,|\s*$)/g);
 
@@ -96,67 +173,16 @@ export default {
                       );
 
                       const shortenedCleanedResult = cleanedResult.slice(0, 13);
-                      mainThis.firstFinalBase.push(shortenedCleanedResult);
+
+                      if (event.target.id === "firstInput") {
+                        mainThis.firstFinalBase.push(shortenedCleanedResult);
+                      } else {
+                        mainThis.secondFinalBase.push(shortenedCleanedResult);
+                      }
                     } else {
                       console.error("No match found for:", platte);
                     }
                   }
-
-                  // TODO: Im mainThis.firstFinalBase müssen die items in key-value-Paare umgewandelt werden, bei denen der key die ID ist
-                  // TODO: und der value ein object, der die restlichen Daten der Platte enthält
-
-                  // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-                  // const request = indexedDB.open("CSV-Databases", 1);
-                  // request.onupgradeneeded = (event) => {
-                  //   mainThis.firstNewDatabase = event.target.result;
-                  //   if (
-                  //     !mainThis.firstNewDatabase.objectStoreNames.contains("myStore")
-                  //   ) {
-                  //     mainThis.firstNewDatabase.createObjectStore("myStore", {
-                  //       keyPath: "id",
-                  //       autoIncrement: true,
-                  //     });
-                  //   }
-                  // };
-                  // request.onsuccess = (event) => {
-                  //   mainThis.firstNewDatabase = event.target.result;
-                  //   console.log("Database opened successfully");
-                  //   console.log(event);
-                  //   console.log(event.target);
-
-                  //   // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-
-                  //   const transaction = mainThis.firstNewDatabase.transaction(
-                  //     ["myStore"],
-                  //     "readwrite"
-                  //   );
-                  //   const store = transaction.objectStore("myStore");
-                  //   store.add({ content });
-
-                  //   transaction.oncomplete = () => {
-                  //     console.log("Data added");
-                  //     const transaction = mainThis.firstNewDatabase.transaction(
-                  //       ["myStore"],
-                  //       "readonly"
-                  //     );
-                  //     const store = transaction.objectStore("myStore");
-                  //     const request = store.get(1);
-
-                  //     request.onsuccess = (event) => {
-                  //       // console.log(event.target.result.content);
-                  //       const regex = /(?=\d{9},)/g;
-                  //       mainThis.firstFinalBase =
-                  //         event.target.result.content.split(regex);
-                  //     };
-                  //   };
-
-                  //   // :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-                  // };
-
-                  // request.onerror = (event) => {
-                  //   console.error("Database error:", event.target.errorCode);
-                  // };
                 });
               });
             })
@@ -167,12 +193,22 @@ export default {
         reader.readAsArrayBuffer(file);
       }
     },
-    clearFile() {
-      this.firstNewDatabase = null;
-      this.firstFinalBase = [];
+    clearFile(whichOne) {
+      if (whichOne === "first") {
+        (this.firstFileName = ""), (this.firstNewDatabase = null);
+        this.firstFinalBase = [];
 
-      if (this.fileInputElement) {
-        this.fileInputElement.value = "";
+        if (this.firstInputElement) {
+          this.firstInputElement.value = "";
+        }
+      } else {
+        this.secondFileName = "";
+        this.secondNewDatabase = null;
+        this.secondFinalBase = [];
+
+        if (this.secondInputElement) {
+          this.secondInputElement.value = "";
+        }
       }
     },
   },
@@ -185,6 +221,27 @@ export default {
 <style scoped>
 div {
   margin-top: 5rem;
+}
+
+.input-area,
+.first-input-area,
+.second-input-area {
+  margin-top: 0;
+}
+
+.second-input-area {
+  margin-block: 2rem;
+}
+
+label {
+  margin-right: 1rem;
+  font-weight: 600;
+  font-size: 1.5rem;
+}
+
+span {
+  color: red;
+  text-decoration: underline;
 }
 
 table {
@@ -200,5 +257,6 @@ td {
 button {
   border: 1px solid black;
   margin-right: 1rem;
+  margin-top: 2rem;
 }
 </style>
